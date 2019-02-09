66a9ebc7d028688bd16a3b621903e48d8ef473cd
Tomek A <tomus.adamczyk@gmail.com>
2019-02-09 18:04:52 +0100

    Inserting and displaying words by single user

--- views/mainview/sidebar/sidebar.php
+++ views/mainview/sidebar/sidebar.php
@@ -5,21 +5,18 @@
     <span class="logo-dot logo-dot--blue"></span>
 </div>
     <div class="cz-sidebar-link">
-        <a href="#" class="cz-sidebar-link__item">Ćwiczenie</a>
+        <a href="#" class="cz-sidebar-link__item">Ćwiczenia</a>
         <div class="cz-sidebar-link-dropdown">
             <div class="cz-sidebar-link-dropdown-link">
-                <a href="#" class="cz-sidebar-link-dropdown__item">Ćwiczenie</a>
+                <a href="#" class="cz-sidebar-link-dropdown__item">Fiszki</a>
             </div>
             <div class="cz-sidebar-link-dropdown-link">
-                <a href="#" class="cz-sidebar-link-dropdown__item">Ćwiczenie</a>
-            </div>
-            <div class="cz-sidebar-link-dropdown-link">
-                <a href="#" class="cz-sidebar-link-dropdown__item">Ćwiczenie</a>
+                <a href="#" class="cz-sidebar-link-dropdown__item">Wisielec</a>
             </div>
         </div>
     </div>
     <div class="cz-sidebar-link">
-        <a href="#" class="cz-sidebar-link__item">Ćwiczenie</a>
+        <a href="" class="cz-sidebar-link__item" data-toggle="modal" data-target="#addingNweWordModal">Dodaj słówko</a>
     </div>
     <div class="cz-sidebar-link">
         <a href="#" class="cz-sidebar-link__item">Ćwiczenie</a>
