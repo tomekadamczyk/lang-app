f1856291314f99d5900eb9704b6c8e4cfa3fe823
Tomek A <tomus.adamczyk@gmail.com>
2019-02-18 07:33:59 +0100

    Styling adjustments

--- views/excercises/flashcards.php
+++ views/excercises/flashcards.php
@@ -6,30 +6,16 @@
 <section class="cz-flashcards">
     <div class="container">
         <div class="row">
-        <?php
-            // require_once 'modules/functions.php';
-            // $translations = new Translations;
-        ?>
-        <div class="col-md-8 mx-auto cz-flashcards__card">
-            <div class="text-center mx-auto translateBox">
-                <h1>Kliknij i przetłumacz</h1>
-                <form action="" method="POST">
-                <input name="randomWord" type="submit" id="randomWord" style="display: none">
-                <div class="btn btn-lg btn-primary" onclick="document.getElementById('randomWord').click();">Wylosuj słowo</div>
-                </form>
-                
-                <?php
-                /*$translations->translateWord();*/
-                ?>
-                <div class="row subject">
-                <p class="wordSubject"></p>
-                </div>
-                <div class="row wordToTranslate"><h2 class="wordToTranslate_item"></h2>
-                <h2 class="wordTranslation"></h2>
-                </div>
-                <div class="btn btn-sm btn-outline checkTranslation">Nie pamiętasz tego słowa?<br><strong>Zobacz tłumaczenie</strong></div>
-                
-                <!-- <button id="chooseWord">Klik</button> -->
+            <div class="col-md-8 mx-auto cz-flashcards__card">
+                <div class="text-center mx-autotranslateBox">
+                    <h1>Kliknij i przetłumacz</h1>
+                    <div class="btn btn-lg btn-primary my-2 " id="cz-flashcards-button">Wylosuj słowo</div>
+                    <div class="text-center my-3">
+                        <p id="cz-flashcards-subject" class="cz-flashcards-subject"></p>
+                    </div>
+                    <div id="flash-word" class="cz-flashcards-to-translate cz-flashcards-to-translate--word"></div>
+                    <div id="flash-translation" class="cz-flashcards-to-translate cz-flashcards-to-translate--translation"></div>
+                    <div class="btn btn-sm btn-outline">Nie pamiętasz tego słowa?<br><span id="cz-flashcards-check-translation"><strong>Zobacz tłumaczenie</strong></span></div>
                 </div>
             </div>
         </div>
